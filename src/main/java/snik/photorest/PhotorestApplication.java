e80a6742f90829d7eba3dbda15be65cb905542a7
Ihar Dubadzelau <igor@dubodeloff.xyz>
2019-01-01 18:40:56 -0500

    init project

---
+++ src/main/java/snik/photorest/PhotorestApplication.java
@@ -0,0 +1,14 @@
+package snik.photorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotorestApplication.class, args);
+	}
+
+}
+
