6c55531a77d3a37930722b04d0a49baf325a35c8
Ihar Dubadzelau <igor@dubodeloff.xyz>
2019-01-01 23:35:09 -0500

    Update Web: for use data source

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,6 @@
+var messageApi = Vue.resource('/message{/id}')
+
+
 Vue.component('message-row', {
     props: ['message'],
     template: '<div><i>({{message.id}})</i>{{ message.text }}</div>'
@@ -5,17 +8,20 @@ Vue.component('message-row', {
 
 Vue.component('messages-list', {
     props: ['messages'],
-    template: '<div><message-row v-for="message in messages" :key="message.id" :message="message"></message-row></div>'
+    template: '<div><message-row v-for="message in messages" :key="message.id" :message="message"></message-row></div>',
+    created: function () {
+        messageApi.get().then( result =>
+            result.json().then( data =>
+                data.forEach( message => this.messages.push(message))
+            )
+        )
+    }
 });
 
 var app = new Vue({
     el: '#app',
     template: '<messages-list :messages="messages"/>',
     data: {
-        messages: [
-            { id: '123', text: 'Wow'},
-            { id: '23', text: 'More'},
-            { id: '3', text: 'Another'}
-        ]
+        messages: []
     }
 });
